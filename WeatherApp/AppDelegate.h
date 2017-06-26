2037c8eb050aadbd24eb0c6bca426ecc2fa83f16
Dinesh Selvaraj <dineshselvaraj@Dineshs-MacBook-Pro.local>
2017-06-26 15:36:05 -0400

    Initial Commit

---
+++ WeatherApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WeatherApp
+//
+//  Created by Dinesh Selvaraj on 6/26/17.
+//  Copyright © 2017 Dinesh Selvaraj. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ WeatherApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WeatherApp
+//
+//  Created by Dinesh Selvaraj on 6/26/17.
+//  Copyright © 2017 Dinesh Selvaraj. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
